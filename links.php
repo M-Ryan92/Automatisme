0a1cdc9ad23765bf8bac7cc85b1f4b4ed45768bb
Teije van Sloten <teije.van.sloten@hva.nl>
2015-04-12 21:11:38 +0200

    prijslijst changed.

--- links.php
+++ links.php
@@ -1,161 +1,161 @@
 <html>
-	<head>
-		<title>Rijschool Automatisme - Links</title>
-		<link href="Bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" type="text/css" href="css/base.css">
-		<style type="text/css">
-			 .minheight {
-			 	min-height: 170px;
-			 }
-			 .tumb-center {
-				text-align: center;
-				display: table-cell;
-				position: absolute;
-				top: 50%;
-				left: 50%;
-				transform: translate(-50%,-50%);
-				-webkit-transform: translate(-50%,-50%);
-				-moz-transform: translate(-50%,-50%);
-				-ms-transform: translate(-50%,-50%);
-			 }
-		</style>
-	</head>
-	<body>
-		<?php include 'header.php';?>
+    <head>
+        <title>Rijschool Automatisme - Links</title>
+        <link href="Bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="css/base.css">
+        <style type="text/css">
+            .minheight {
+                min-height: 170px;
+            }
+            .tumb-center {
+                text-align: center;
+                display: table-cell;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%,-50%);
+                -webkit-transform: translate(-50%,-50%);
+                -moz-transform: translate(-50%,-50%);
+                -ms-transform: translate(-50%,-50%);
+            }
+        </style>
+    </head>
+    <body>
+        <?php include 'header.php'; ?>
 
-		<div class="container">
-			<div class="row">
-				<div class="col-md-10 col-md-offset-1">
-					<a href="https://mijn.cbr.nl/nl/Authentication/Login?ReturnUrl=%2fnl" class="thumbnail col-md-2 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/CBR.jpg" class="col-md-12" />
-							<p class="text-center">Theorie reserveren</p>
-						</div>
-					</a>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <a href="https://mijn.cbr.nl/nl/Authentication/Login?ReturnUrl=%2fnl" class="thumbnail col-md-2 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/CBR.jpg" class="col-md-12" />
+                            <p class="text-center">Theorie reserveren</p>
+                        </div>
+                    </a>
 
-					<a href="https://mijn.cbr.nl/nl/Authentication/Login?ReturnUrl=%2f" class="thumbnail col-md-2 col-md-offset-1 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/CBR.jpg" class="col-md-12" />
-							<p class="text-center">Rijschool machtigen</p>
-						</div>
-					</a>
+                    <a href="https://mijn.cbr.nl/nl/Authentication/Login?ReturnUrl=%2f" class="thumbnail col-md-2 col-md-offset-1 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/CBR.jpg" class="col-md-12" />
+                            <p class="text-center">Rijschool machtigen</p>
+                        </div>
+                    </a>
 
-					<a href="http://www.rijschoolgegevens.nl/index.asp?pageid=2&gemeente=0&examenplaats=33&rijschoolid=9736#9736" class="thumbnail col-md-2 col-md-offset-1 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/CBR.jpg" class="col-md-12" />
-							<p class="text-center">Actuele slagingspercentage</p>
-						</div>
-					</a>
+                    <a href="http://www.rijschoolgegevens.nl/index.asp?pageid=2&gemeente=0&examenplaats=33&rijschoolid=9736#9736" class="thumbnail col-md-2 col-md-offset-1 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/CBR.jpg" class="col-md-12" />
+                            <p class="text-center">Actuele slagingspercentage</p>
+                        </div>
+                    </a>
 
-					<a href="http://www.cbr.nl/praktijkfilm-auto.pp" class="thumbnail col-md-2 col-md-offset-1 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/CBR.jpg" class="col-md-12" />
-							<p class="text-center">Zo gaat het praktijkexamen</p>
-						</div>
-					</a>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-10 col-md-offset-1">
-					<a href="http://www.2todrive.nl/jongeren/home" class="thumbnail col-md-2 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/2todrive.jpg" class="col-md-12" />
-							<p class="text-center">2ToDrive</p>
-						</div>
-					</a>
+                    <a href="http://www.cbr.nl/praktijkfilm-auto.pp" class="thumbnail col-md-2 col-md-offset-1 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/CBR.jpg" class="col-md-12" />
+                            <p class="text-center">Zo gaat het praktijkexamen</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <a href="http://www.2todrive.nl/jongeren/home" class="thumbnail col-md-2 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/2todrive.jpg" class="col-md-12" />
+                            <p class="text-center">2ToDrive</p>
+                        </div>
+                    </a>
 
-					<a href="http://www.2todrive.nl/page/begeleiderspas-aanvragen" class="thumbnail col-md-2 col-md-offset-1 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/2todrive.jpg" class="col-md-12" />
-							<p class="text-center">Begeleiders pas aanvragen</p>
-						</div>
-					</a>
+                    <a href="http://www.2todrive.nl/page/begeleiderspas-aanvragen" class="thumbnail col-md-2 col-md-offset-1 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/2todrive.jpg" class="col-md-12" />
+                            <p class="text-center">Begeleiders pas aanvragen</p>
+                        </div>
+                    </a>
 
-					<a href="http://www.purmerend.nl/loket/node/102" class="thumbnail col-md-2 col-md-offset-1 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/rijbewijsgehaald.jpg" class="col-md-12" />
-							<p class="text-center">Rijbewijs aanvragen Purmerend</p>
-						</div>
-					</a>
+                    <a href="http://www.purmerend.nl/loket/node/102" class="thumbnail col-md-2 col-md-offset-1 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/rijbewijsgehaald.jpg" class="col-md-12" />
+                            <p class="text-center">Rijbewijs aanvragen Purmerend</p>
+                        </div>
+                    </a>
 
-					<a href="http://www.rijschoolregister.nl" class="thumbnail col-md-2 col-md-offset-1 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/RijschoolRegister.jpg" class="col-md-12" />
-							<p class="text-center">Rijschool Register</p>
-						</div>
-					</a>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-10 col-md-offset-1">
+                    <a href="http://www.rijschoolregister.nl" class="thumbnail col-md-2 col-md-offset-1 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/RijschoolRegister.jpg" class="col-md-12" />
+                            <p class="text-center">Rijschool Register</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
 
-					<a href="http://www.theorie-leren.nl" class="thumbnail col-md-2 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/VEKABEST.jpg" class="col-md-12" />
-							<p class="text-center">Theorie leren</p>
-						</div>
-					</a>
+                    <a href="http://www.theorie-leren.nl" class="thumbnail col-md-2 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/VEKABEST.jpg" class="col-md-12" />
+                            <p class="text-center">Theorie leren</p>
+                        </div>
+                    </a>
 
-					<a href="https://www.facebook.com/automatisme.nl" class="thumbnail col-md-2 col-md-offset-1 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/fbicon.png" class="col-md-12" />
-							<p class="text-center">Automatisme</p>
-						</div>
-					</a>
+                    <a href="https://www.facebook.com/automatisme.nl" class="thumbnail col-md-2 col-md-offset-1 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/fbicon.png" class="col-md-12" />
+                            <p class="text-center">Automatisme</p>
+                        </div>
+                    </a>
 
-					<a href="https://twitter.com/automatisme_nl" class="thumbnail col-md-2 col-md-offset-1 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/twiticon.png" class="col-md-12" />
-							<p class="text-center">Automatisme</p>
-						</div>
-					</a>
+                    <a href="https://twitter.com/automatisme_nl" class="thumbnail col-md-2 col-md-offset-1 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/twiticon.png" class="col-md-12" />
+                            <p class="text-center">Automatisme</p>
+                        </div>
+                    </a>
 
-					<a href="https://digid.nl/inloggen" class="thumbnail col-md-2 col-md-offset-1 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/digid.png" class="col-md-12" />
-							<p class="text-center">DigiD aanvragen</p>
-						</div>
-					</a>
+                    <a href="https://digid.nl/inloggen" class="thumbnail col-md-2 col-md-offset-1 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/digid.png" class="col-md-12" />
+                            <p class="text-center">DigiD aanvragen</p>
+                        </div>
+                    </a>
 
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-10 col-md-offset-1">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
 
-					<a href="http://www.rijschoolbelang.nl" class="thumbnail col-md-2 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/SRB.jpg" class="col-md-12" />
-							<p class="text-center">SRB</p>
-						</div>
-					</a>
+                    <a href="http://www.rijschoolbelang.nl" class="thumbnail col-md-2 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/SRB.jpg" class="col-md-12" />
+                            <p class="text-center">SRB</p>
+                        </div>
+                    </a>
 
-					<a href="http://www.hetnieuwerijden.nl/home" class="thumbnail col-md-2 col-md-offset-1 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/hetnieuwerijden.jpg" class="col-md-12" />
-							<p class="text-center">Het nieuwe rijden</p>
-						</div>
-					</a>
+                    <a href="http://www.hetnieuwerijden.nl/home" class="thumbnail col-md-2 col-md-offset-1 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/hetnieuwerijden.jpg" class="col-md-12" />
+                            <p class="text-center">Het nieuwe rijden</p>
+                        </div>
+                    </a>
 
-					<a href="" class="thumbnail col-md-2 col-md-offset-1 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/algemenevoorwaarden.jpg" class="col-md-12" />
-							<p class="text-center">Algemene voorwaarden</p>
-						</div>
-					</a>
+                    <a href="" class="thumbnail col-md-2 col-md-offset-1 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/algemenevoorwaarden.jpg" class="col-md-12" />
+                            <p class="text-center">Algemene voorwaarden</p>
+                        </div>
+                    </a>
 
-					<a href="http://vvn.nl/" class="thumbnail col-md-2 col-md-offset-1 minheight">
-						<div class="col-md-12 tumb-center">
-							<img src="img/veiligverkeer.jpg" class="col-md-12" />
-							<p class="text-center">Veilig verkeer nederland</p>
-						</div>
-					</a>
+                    <a href="http://vvn.nl/" class="thumbnail col-md-2 col-md-offset-1 minheight">
+                        <div class="col-md-12 tumb-center">
+                            <img src="img/veiligverkeer.jpg" class="col-md-12" />
+                            <p class="text-center">Veilig verkeer nederland</p>
+                        </div>
+                    </a>
 
-				</div>
-			</div>
-		</div>
-		<?php include 'footer.php';?>
-	</body>
-	<script type="text/javascript" src="JQuery/jquery.min.js"></script>
-	<script type="text/javascript" src="Bootstrap/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/footermargin.js"></script>
+                </div>
+            </div>
+        </div>
+        <?php include 'footer.php'; ?>
+    </body>
+    <script type="text/javascript" src="JQuery/jquery.min.js"></script>
+    <script type="text/javascript" src="Bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/footermargin.js"></script>
 </html>
